af065d352ccee5c9c008ca0be429a81f5cc0ca02
Konstantin Beloglazov <ajoq@ya.ru>
2021-12-02 11:57:39 +0300

    done v1

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-console.log('Hello, ajoq');
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(player) {
+    if (this.members.has(player)) {
+      throw new Error('Игрок уже в команде');
+    } else {
+      this.members.add(player);
+    }
+  }
+
+  addAll(...players) {
+    players.forEach((item) => this.members.add(item));
+  }
+
+  toArray() {
+    const playersArr = [];
+    this.members.forEach((item) => playersArr.push(item));
+    return playersArr;
+  }
+}
